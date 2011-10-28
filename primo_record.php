4641f2ee014f0b298f4666769dcd141f2f20110a
Kevin Reiss <kevin.reiss@gmail.com>
2011-10-28 19:15:36 -0400

    add getLinks() function to return all links stored in PNX

--- primo_record.php
+++ primo_record.php
@@ -1,6 +1,7 @@
 <?php
 
-Class PrimoRecord {
+Class PrimoRecord 
+{
   
   private $xpath_base = "//sear:DOC[1]//";
   private $xpath_doc_root = "//sear:DOC[1]";
@@ -28,20 +29,22 @@ Class PrimoRecord {
     if (!is_null($full_primo_record)) {
       //echo $full_primo_record->saveXML();
       foreach ($full_primo_record as $element) {
-       $primo_record_string .= "<br/>[". $element->nodeName. "]";
+       $primo_record_string .= "[". $element->nodeName. "]";
         $nodes = $element->childNodes;
         foreach ($nodes as $node) {
-          $primo_record_string .= "<br/>[" . $node->nodeName . "] = " . $node->nodeValue. "\n";
+          $primo_record_string .= "[" . $node->nodeName . "] = " . $node->nodeValue. "\n";
         }
       }
     } else {
       $primo_record_string .= "[No Record Found]";
     }
+    
     return $primo_record_string;
   }
   
   private function loadXPath($xml) {
     $dom = DOMDocument::loadXML($xml);
+    
     return new DOMXPath($dom);
   }
   
@@ -51,11 +54,12 @@ Class PrimoRecord {
   
   private function query($path) {
     //echo $this->xpath_base.$path."\n";
+    
     return $this->xpath->query($this->xpath_base.$path);
   }
   
   // get the contents of one specific tag
-  public function getText($tag) {
+  private function getText($tag) {
     $textContent = '';
     $is_namespace = '/\w+:\w+/';
     if(!(preg_match($is_namespace,$tag))) {
@@ -65,6 +69,7 @@ Class PrimoRecord {
     foreach ($nodeList as $node) {
       $textContent .= ' ' . $node->textContent;
     }
+    
     return $textContent;
   }
   
@@ -75,31 +80,72 @@ Class PrimoRecord {
     foreach ($nodeList as $node) {
       $textContent .= ' ' . $node->textContent;
     }
+    
     return $textContent;
   }
   
+  /*
+   * Returns all links from the sear:Links segment by category
+   * 
+   */
+  public function getAllLinks() {
+    $links = array();
+    $source_path = "sear:LINKS//*";
+    $nodeList = $this->query($source_path);
+    foreach ($nodeList as $node) {
+      array_push($links, $node->textContent);
+    }
+    
+    return $links;
+  }
+  
+  public function getOpenURL() {
+    
+  }
+  
+  /*
+   *  getSourceIDs
+   * 
+   *  extracts record source ID from PNX record
+   *  
+   *  Example Text
+   *  looks like $$V6109368$$OPRN_VOYAGER6109368  
+   */
+  
   public function getSourceIDs() {
-    //$source_path = 
     $source_ids = array();
     $source_path = "def:PrimoNMBib/def:record/def:control/def:sourcerecordid";
+    $id_delimiter = '/\$\$O/';
     $sourceList = $this->query($source_path);
-    $id_count = $sourceList->length; // how many sources did this rec have?
-    foreach ($sourceList as $node) {
-      //looks like $$V6109368$$OPRN_VOYAGER6109368     
+    $id_count = $sourceList->length; 
+    foreach ($sourceList as $node) {    
       if ($id_count == 1) {
         array_push($source_ids, $this->institutionID.$node->textContent);
       } else {
-        $id_delimiter = '/\$\$O/';
         $id = preg_split($id_delimiter,$node->textContent);
         array_push($source_ids,$id[1]);
       }
       
     }
+    
     return $source_ids;
   }
   
+  /*
+   * getAvailableLibraries
+   * 
+   * Looks at PNX Records and extracts PNX stored Locations for single 
+   * source and merged records. From the "availlibrary" element
+   * 
+   *  Single Source Example Availibrary Text
+   *  have to split this $$IPRN$$LFIRE$$1(F)$$2PS3618.A914 B36 2010$$Savailable$$31$$40$$5N$$60$$Xprincetondb$$Yf
+   *  
+   *  Merged Record Example Aviallibrary Text
+   *  have to split this <aviallibrary>$$IPRN$$LFIRE$$1(F)$$2D810.C696 P644 2000$$Savailable$$31$$40$$5N$$619$$Xprincetondb$$Yf$$OPRN_VOYAGER3216675</availlibrary>
+   *  
+   */
+  
   public function getAvailabilbleLibraries() {
-    //$source_path = 
     $available_ids = array();
     $available_path = "def:PrimoNMBib/def:record/def:display/def:availlibrary";
     $availableList = $this->query($available_path);
@@ -108,16 +154,9 @@ Class PrimoRecord {
     $id_delimeter = '/\$\$O/';
     foreach ($availableList as $node) {
       if($num_avail_items == 1) {
-        //echo $node->textContent;
-        // have to split this $$IPRN$$LFIRE$$1(F)$$2PS3618.A914 B36 2010$$Savailable$$31$$40$$5N$$60$$Xprincetondb$$Yf
         $loc_code = preg_split($location_code_delimiter, $node->textContent);
-        //echo $this->getRecordID();
-        //echo $loc_code[1];
         $available_ids[$this->getRecordID()] = array($loc_code[1]);
-         // only one to send back
-      }
-      else {
-        // have to split this <aviallibrary>$$IPRN$$LFIRE$$1(F)$$2D810.C696 P644 2000$$Savailable$$31$$40$$5N$$619$$Xprincetondb$$Yf$$OPRN_VOYAGER3216675</availlibrary>
+      } else {
         $location_and_sourceid = preg_split($location_code_delimiter, $node->textContent);
         $available_string = preg_split($id_delimeter, $location_and_sourceid[1]);
         if (array_key_exists($available_string[1], $available_ids)) {
@@ -127,9 +166,9 @@ Class PrimoRecord {
         }
       }
     }
+
     return $available_ids;
   }
   
 }
-
 ?>
--- primo_record_test.php
+++ primo_record_test.php
@@ -3,14 +3,14 @@ require_once('primo_record.php');
 
 $record = new PrimoRecord(file_get_contents('getit-response.xml'));
 
-print $record;
+//print $record;
 
 //echo '[', $record->getText('recordid'), "]\n";
 //echo '[', $record->getText('openurl'), "]\n";
 //echo '[', $record->getText('sear:openurl'), "]\n";
 print_r($record->getAvailabilbleLibraries());
 //echo '[', $record->getText('availlibrary'), "]\n";
-
+print_r($record->getAllLinks());
 //print_r($record->getSourceIDs());
 //echo $record->getRecordID();
 
