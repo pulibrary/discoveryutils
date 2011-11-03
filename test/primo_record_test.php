cd2f07eecf0c98105f681b9a4b795c267563c0fe
Kevin Reiss <kevin.reiss@gmail.com>
2011-11-03 11:01:41 -0400

    refactor directory structure, add retrieval methods and tests

--- test/primo_record_test.php
+++ test/primo_record_test.php
@@ -1,5 +1,6 @@
 <?php
 require_once('../classes/primo_record.php');
+require_once('../classes/primo_parser.php');
 
 $record = new PrimoRecord(file_get_contents('./support/getit-response.xml'));
 
@@ -8,18 +9,30 @@ $record = new PrimoRecord(file_get_contents('./support/getit-response.xml'));
 //echo '[', $record->getText('recordid'), "]\n";
 //echo '[', $record->getText('openurl'), "]\n";
 //echo '[', $record->getText('sear:openurl'), "]\n";
-print_r($record->getAvailabilbleLibraries());
+//print_r($record->getAvailabilbleLibraries());
 //echo '[', $record->getText('availlibrary'), "]\n";
-print_r($record->getAllLinks());
+print_r($record->getBriefInfo());
 //print_r($record->getSourceIDs());
 //echo $record->getRecordID();
 
 $single_source = new PrimoRecord(file_get_contents('./support/single_voyager_source.xml'));
 //print_r($single_source->getSourceIDs());
 //echo $single_source->getRecordID();
-print_r($single_source->getAvailabilbleLibraries());
+//print_r($single_source->getAvailabilbleLibraries());
 
 $dedup = new PrimoRecord(file_get_contents('./support/dedup_response.xml'));
-print_r($dedup->getAvailabilbleLibraries());
+//print_r($dedup->getAvailabilbleLibraries());
+//print_r($dedup->getAllLinks());
+//print_r($dedup->getGetItLinks());
+print_r($dedup->getBriefInfo());
+$PNXloader = new PNXLoader();
+$xml = file_get_contents('./support/single_voyager_source.xml');
+$pnx_rec = $PNXloader->loadPNX($xml);
+
+//print_r($pnx_rec->getAllLinks());
+//print_r($pnx_rec->getGetItLinks());
+print_r($pnx_rec->getAvailabilbleLibraries());
+print_r($pnx_rec->getBriefInfo());
+//print_r($pnx_rec->getFullText());
 
 ?>
