e5dbb8f294979de450995f0d635273be9dcdc90b
Kevin Reiss <kevin.reiss@gmail.com>
2011-11-03 11:03:24 -0400

    refactor directory structure, add retrieval methods and tests

--- classes/primo_parser.php
+++ classes/primo_parser.php
@@ -1,18 +1,34 @@
 <?php
 
 /* 
- * PrimoPNXParser
+ * PrimoPNXLoad
  *
  * Generates Primo PNX Record objects
  *
  */
 
-class PrimoPNXParser 
+class PNXLoader 
 {
-
+  
+  public static function loadPNX($xml) {
+    return new PrimoRecord($xml);
+  }
+  
 }
 
-class PrimoPNXParseException Extends Exception 
+class PrimoClient
+{
+  const PRIMO_BASE_URL = "http://searchit.princeton.edu/PrimoWebServices/xservice/getit";
+  const PRIMO_INSTITUTION = "PRN";
+  
+  public static function getIDRequest($pnx_id) {
+    $xml = file_get_contents(PRIMO_BASE_URL ."?institution=" . PRIMO_INSTITUTION ."&docId=".$pnx_id);
+    
+    return $xml;
+  }
+}
+
+class PrimoLoadException Extends Exception 
 {
 
 }
---
+++ web/index.php
@@ -0,0 +1,22 @@
+<?php
+require_once('primo_record.php');
+header('Content-type: application/json');
+
+$primo_base_url = "http://searchit.princeton.edu/PrimoWebServices/xservice/getit";
+$primo_institution = "PRN";
+
+if(isset($_GET['doc_id'])) {
+  $pnx_id = $_GET['doc_id'];
+  $xml = file_get_contents($primo_base_url."?institution=".$primo_institution."&docId=".$pnx_id);
+  //echo $primo_base_url."?instiution=".$primo_institution."&docId=".$pnx_id;
+  $record = new PrimoRecord($xml);
+  $source_ids = $record->getAvailabilbleLibraries();
+
+  echo json_encode($source_ids);
+} else {
+  $empty_response = array("message" => "No ID Supplied");
+  echo json_encode($empty_response);
+}
+
+?>
+
