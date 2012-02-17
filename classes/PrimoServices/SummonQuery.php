10a52f54789840a9cce6d133b94a83fbee2cacb6
Kevin Reiss <kevin.reiss@gmail.com>
2012-02-16 20:05:05 -0500

    Add Summon Query class

---
+++ classes/PrimoServices/SummonQuery.php
@@ -0,0 +1,35 @@
+<?php
+namespace PrimoServices;
+
+class SummonQuery {
+  
+  private $summon_base = "http://princeton.summon.serialssolutions.com/?";
+  private $query_key = "s.q";
+  private $query;
+  private $limiters;
+  private $url;
+  
+  
+  function __construct($query, $limiters = array()) {
+    $this->limiters = $limiters;
+    $this->query = $query;
+    $this->buildLink();
+  }
+  
+  private function buildLink() {
+    $query_string = array("{$this->query_key}={$this->query}");   
+    if(count($this->limiters > 0)) {
+      foreach($this->limiters as $limiter => $limit_value) {
+        array_push($query_string, "{$limiter}={$limit_value}");
+      }
+    }
+    $this->url = $this->summon_base . implode('&', $query_string);
+  }
+  
+  public function getLink() {
+    return $this->url;
+  }
+}
+
+
+?>
---
+++ tests/LookupApp/Tests/SummonQueryTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace LookupApp\Tests;
+
+/**
+ * Tests "Summon Query Links"
+ */
+
+ 
+class SummonQueryTest extends \PHPUnit_Framework_TestCase {
+  protected function setUp() {
+    $this->summon_base_url = "http://princeton.summon.serialssolutions.com/?";
+    $this->summon_query_param = "s.q=";
+  }
+  
+  
+  function testIsValidSummonQuery() {
+    $query_strings = array("dogs", "dogs and cats", "Diwight Eisenhower");
+    foreach($query_strings as $query) {
+      $summon_query = new \PrimoServices\SummonQuery($query);
+      $this->assertEquals($summon_query->getLink(), $this->summon_base_url . $this->summon_query_param . $query);
+    }
+  }
+  
+}
+
+?>
