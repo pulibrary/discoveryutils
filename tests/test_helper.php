8a2ecbcb1c12964d71f2622ce9fdfcc3e2d894c5
Kevin Reiss <kevin.reiss@gmail.com>
2012-08-11 13:22:06 -0400

    simplfy test helper

--- tests/test_helper.php
+++ tests/test_helper.php
@@ -1,11 +1,3 @@
 <?php
-  //require_once __DIR__.'/../vendor/silex.phar'; // include silex
   require_once __DIR__.'/../vendor/autoload.php'; 
-  set_include_path(get_include_path() . PATH_SEPARATOR . $basepath);
-
-  // Let's just load all PHP files in the classes directory in case
-  // we need them.
-  foreach (glob($basepath.'*.php') as $filename) {
-    require_once(realpath($filename));
-  }
 ?>
