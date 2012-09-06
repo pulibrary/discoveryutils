bc42bb454ed56970fa60ed7a6e1df0af611dd5ba
Kevin Reiss <kevin.reiss@gmail.com>
2012-09-06 10:03:51 -0400

    test cleanup

--- tests/LookupApp/Tests/HTTPClientTest.php
+++ tests/LookupApp/Tests/HTTPClientTest.php
@@ -2,6 +2,7 @@
 namespace LookupApp\Tests;
 
 use Guzzle\Service\Client;
+//use Guzzle\Http\Client;
 
 /*
  * Test should pass if you are running code from machine
@@ -12,11 +13,33 @@ use Guzzle\Service\Client;
 class HTTPClientTest extends  \PHPUnit_Framework_TestCase
 {
     protected function setUp() {
-       $this->client = new \Guzzle\Service\Client('http://libwebprod.princeton.edu/searchit');
+       //$this->client = new \Guzzle\Service\Client('http://libwebprod.princeton.edu/searchit');
+       $this->primo_xservice = new \Guzzle\Service\Client("http://searchit.princeton.edu/PrimoWebServices/xservice/search/brief");
     }
     
     function testBaseURLSetting() {
-      $response = $this->client->get('find/any/cats?limit=exact')->send();
+      //$response = $this->client->get('find/any/cats?limit=exact')->send();
       //echo $response->getBody();
     }
+    
+    function testPrimoBasicQuery() {
+      $response = $this->primo_xservice->get("?institution=PRN&onCampus=false&indx=1&dym=true&highlight=true&displayField=title&query=title,exact,journal+of+politics&bulkSize=3&loc=local,scope:(PRN)")->send();
+      //echo (string)$response->getBody();
+    }
+    
+    function testPrimoFacetQuery() {
+      $request = $this->primo_xservice->get("?institution=PRN&onCampus=false&indx=1&dym=true&highlight=true&displayField=title&query=title,exact,journal+of+politics&bulkSize=3&loc=local,scope:(PRN)");
+      $request->getQuery()->add('query', 'facet_rtype,exact,journals');
+      $request->getQuery()->setAggregateFunction(array($request->getQuery(), 'aggregateUsingDuplicates'));
+      $response = $request->send();
+    }
+    
+    function testFileGetContents() {
+      $base = "http://searchit.princeton.edu/PrimoWebServices/xservice/search/brief?";
+      //$without_facet = file_get_contents($base . "institution=PRN&onCampus=false&indx=1&dym=true&highlight=true&displayField=title&query=title,exact,journal+of+politics&bulkSize=3&loc=local,scope:(PRN)");
+      //print_r($without_facet);
+      //$with_facet = file_get_contents($base . "institution=PRN&onCampus=false&indx=1&dym=true&highlight=true&displayField=title&query=title,exact,journal+of+politics&bulkSize=3&loc=local,scope:(PRN)&query=facet_rtype,exact,jouranls");
+      //print_r($with_facet);
+    }
+    
 }
