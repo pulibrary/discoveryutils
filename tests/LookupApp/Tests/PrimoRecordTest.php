a1d052ef65c448ad26856faf6d18b1e998cae9d0
Kevin Reiss <kevin.reiss@gmail.com>
2012-09-06 23:28:43 -0400

    more cleanup of from namespace rename

--- tests/LookupApp/Tests/PrimoRecordTest.php
+++ tests/LookupApp/Tests/PrimoRecordTest.php
@@ -15,13 +15,13 @@ class LookupPrimoRecordTest extends \PHPUnit_Framework_TestCase {
       'default_pnx_source_id' => 'PRN_VOYAGER',
     );
     $single_record_reponse = file_get_contents(dirname(__FILE__).'../../../support/PRN_VOYAGER4773991.xml');
-    $this->single_source_record = new \PrimoServices\PrimoRecord($single_record_reponse, $primo_server_connection);
+    $this->single_source_record = new \Primo\Record($single_record_reponse, $primo_server_connection);
     $dedup_record_reponse = file_get_contents(dirname(__FILE__).'../../../support/dedup_response.xml');
-    $this->dedup_source_record = new \PrimoServices\PrimoRecord($dedup_record_reponse, $primo_server_connection);
+    $this->dedup_source_record = new \Primo\Record($dedup_record_reponse, $primo_server_connection);
     $electronic_record_response = file_get_contents(dirname(__FILE__).'../../../support/PRN_VOYAGER5399326.xml');
-    $this->electronic_record_response = new \PrimoServices\PrimoRecord($electronic_record_response,$primo_server_connection);
+    $this->electronic_record_response = new \Primo\Record($electronic_record_response,$primo_server_connection);
     $electronic_record_via_sfx_response = file_get_contents(dirname(__FILE__).'../../../support/PRN_VOYAGER857469.xml');
-    $this->electronic_record_via_sfx_response = new \PrimoServices\PrimoRecord($electronic_record_via_sfx_response,$primo_server_connection);
+    $this->electronic_record_via_sfx_response = new \Primo\Record($electronic_record_via_sfx_response,$primo_server_connection);
   }
   
   function testGetSinglePrintRecordLocations() {
