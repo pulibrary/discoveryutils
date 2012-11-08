923623b9b7c550ca2d45e3f9f83c13987e662fb9
Kevin Reiss <kevin.reiss@gmail.com>
2012-11-08 13:47:46 -0500

    fix failing query test

--- tests/LookupApp/Tests/QueryDeepLinkTest.php
+++ tests/LookupApp/Tests/QueryDeepLinkTest.php
@@ -19,7 +19,7 @@ class QueryDeepLinkTest extends \PHPUnit_Framework_TestCase {
     $query_strings = array("dogs", "cats and (fish meal)", "9781416987031");
     foreach($query_strings as $query) {
       $tab = "location";
-      $link = "http://searchit.princeton.edu/primo_library/libweb/action/dlSearch.do?institution=PRN&onCampus=false&indx=1&bulkSize=10&dym=true&highlight=true&displayField=title&query=any%2Ccontains%2C" . urlencode($query) . "&loc=local,scope:(OTHERS),scope:(FIRE)&vid=PRINCETON&tab={$tab}";
+      $link = "http://searchit.princeton.edu/primo_library/libweb/action/dlSearch.do?institution=PRN&onCampus=false&indx=1&dym=true&highlight=true&displayField=title&query=any%2Ccontains%2C" . urlencode($query) . "&bulkSize=10&loc=local,scope:(OTHERS),scope:(FIRE)&vid=PRINCETON&tab={$tab}";
       $searchlink = new \Primo\SearchDeepLink($query, 'any', 'contains', $primo_server_connection, $tab, $scopes = array("OTHERS","FIRE"));
       //$this->assertEquals(strlen($link), strlen($searchlink->getLink()));
       $this->assertEquals($searchlink->getLink(), $link);
