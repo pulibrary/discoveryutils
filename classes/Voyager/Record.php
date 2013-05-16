2b021edd8ad362a1d0b1ab81af27888337d2d1b4
Kevin Reiss <kevin.reiss@gmail.com>
2013-05-16 11:48:47 -0400

    Add 'Voyager' namespace to composer.json, run composer.phar update for all packages

---
+++ classes/Voyager/Record.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: KevinReiss
+ * Date: 5/16/13
+ * Time: 9:25 AM
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ tests/LookupApp/Tests/VoyagerHoldingsTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: KevinReiss
+ * Date: 5/16/13
+ * Time: 11:27 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace LookupApp\Tests;
+use Voyager\Voyager;
+
+class VoyagerHoldingsTest extends \PHPUnit_Framework_TestCase {
+
+}
