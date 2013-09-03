862d5e87ce15f8a148015dfa457cdac54a4f9a48
Kevin Reiss <kevin.reiss@gmail.com>
2013-09-03 08:59:30 -0400

    Update paths for new Primo install, take out hardcoded scope from various routes

--- src/app.php
+++ src/app.php
@@ -53,12 +53,13 @@ $app['search_tabs'] = array(
 $library_scopes = Yaml::parse(__DIR__.'/../conf/scopes.yml');
 
 $app['primo_server_connection'] = array(
-  'base_url' => 'http://searchit.princeton.edu',
+  'base_url' => 'http://princeton-dc04.hosted.exlibrisgroup.com/',
    //'base_url' => 'http://chiprist01v1.hosted.exlibrisgroup.com:1701/',
   'institution' => 'PRN',
   'default_view_id' => 'PRINCETON',
   'default_pnx_source_id' => 'PRN_VOYAGER',
-  'default.scope' => array("OTHERS","FIRE"),
+  //'default.scope' => array("ALL+PRINCETON+LIBRARIES"),
+  'default.scope' => array("OTHERS","FIRE","RECAP"),
   'default.search' => "contains",
   'num.records.brief.display' => 5,
   'available.scopes' => $library_scopes,
@@ -188,7 +189,7 @@ $app->match('/search/{tab}', function(Request $request, $tab) use($app) {
   } elseif($tab == 'dball') {
     $deep_search_link = new CoreSearchLink($app['library.core']['host'] , $app['library.core']['db.search.path'] , $app->escape($query));
   } else {
-    $deep_search_link = new SearchDeepLink($query, "any", "contains", $app['primo_server_connection'], $tab, array("OTHERS", "FIRE")); //WATCHOUT - Order Matters 
+    $deep_search_link = new SearchDeepLink($query, "any", "contains", $app['primo_server_connection'], $tab, $app['primo_server_connection']['default.scope']); //WATCHOUT - Order Matters 
   }
   $app['monolog']->addInfo("TAB:" . $tab . "\tQUERY:" . $query . "\tREDIRECT:" . $deep_search_link->getLink() . "\tREFERER:" . $referer);
   return $app->redirect($deep_search_link->getLink());
@@ -731,7 +732,7 @@ $app->get('/articles/{index_type}', function($index_type) use($app) {
   } 
   if ($search_results) {
     $response = new PrimoResponse($search_results, $app['primo_server_connection']);
-    $deep_link = new SearchDeepLink($query, $app->escape($index_type), $operator, $app['primo_server_connection'], 'location', array("OTHERS", "FIRE"), $primo_query->getFacets());
+    $deep_link = new SearchDeepLink($query, $app->escape($index_type), $operator, $app['primo_server_connection'], 'location', $app['primo_server_connection']['default.scope'], $primo_query->getFacets());
     $response_data = array(
       'query' => $app->escape($query),
       'number' => $response->getHits(),
