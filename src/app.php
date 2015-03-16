842ee6d9548295e81e7dd0207decd088b6caa5ca
Kevin Reiss <kevin.reiss@gmail.com>
2015-03-16 14:19:26 -0400

    update scopes

--- src/app.php
+++ src/app.php
@@ -51,22 +51,23 @@ $app['search_tabs'] = array(
   array("index" => "mendel", "label" => "Mendel Library Audio"),
 );
 
-$library_scopes = Yaml::parse(__DIR__.'/../conf/scopes.yml');
 
-$app['primo_server_connection'] = array(
-  //'base_url' => 'http://princeton-dc04.hosted.exlibrisgroup.com/',
-  //'base_url' => 'http://chiprist01v1.hosted.exlibrisgroup.com:1701/',
-  'base_url' => 'http://searchit.princeton.edu',
-  'institution' => 'PRN',
-  'default_view_id' => 'PRINCETON',
-  'default_pnx_source_id' => 'PRN_VOYAGER',
-  //'default.scope' => array("ALL+PRINCETON+LIBRARIES"),
-  'default.scope' => array("OTHERS","FIRE","RECAP","EAD"),
-  'default.search' => "contains",
-  'num.records.brief.display' => 5,
-  'available.scopes' => $library_scopes,
-  'record.request.base' => "http://library.princeton.edu/requests/",
-);
+
+$app['primo_server_connection'] = Yaml::parse(__DIR__.'/../conf/primo.yml');
+// = array(
+//   //'base_url' => 'http://princeton-dc04.hosted.exlibrisgroup.com/',
+//   //'base_url' => 'http://chiprist01v1.hosted.exlibrisgroup.com:1701/',
+//   'base_url' => 'http://searchit.princeton.edu',
+//   'institution' => 'PRN',
+//   'default_view_id' => 'PRINCETON',
+//   'default_pnx_source_id' => 'PRN_VOYAGER',
+//   //'default.scope' => array("ALL+PRINCETON+LIBRARIES"),
+//   'default.scope' => array("OTHERS","FIRE","RECAP","EAD"),
+//   'default.search' => "contains",
+//   'num.records.brief.display' => 5,
+//   'available.scopes' => $library_scopes,
+//   'record.request.base' => "http://library.princeton.edu/requests/",
+// );
 
 $app['voyager.connection'] = array(
   'base.url' => "http://catalog.princeton.edu",
