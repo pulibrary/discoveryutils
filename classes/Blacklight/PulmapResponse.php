13f75e00b3ddf2f0cb09255b75645a4d59aa4a3d
Carolyn Cole <cam156@psu.edu>
2019-08-15 09:23:46 -0400

    Adding description to PulmapResponse Additionally fixing the parsing of the blacklight 7 response for type, publisher and author Added a test for the response parsing
    fixes #141

--- classes/Blacklight/PulmapResponse.php
+++ classes/Blacklight/PulmapResponse.php
@@ -24,15 +24,18 @@ class PulmapResponse
     foreach($record_list as $record) {
       $parsed_record = array();
       $parsed_record["title"] = $record["attributes"]["dc_title_s"]["attributes"]["value"];
+      $parsed_record["id"] = $record["id"];
       if (isset($record["attributes"]["dc_creator_sm"])) {
         $parsed_record["author"] = $record["attributes"]["dc_creator_sm"]["attributes"]["value"];
       }
-      if (isset($record["dc_publisher_s"])) {
-        $parsed_record["publisher"] = $record["dc_publisher_s"]["attributes"]["value"];
+      if (isset($record["attributes"]["dc_publisher_s"])) {
+        $parsed_record["publisher"] = $record["attributes"]["dc_publisher_s"]["attributes"]["value"];
       }
-      $parsed_record["id"] = $record["id"];
-      if (isset($record["dc_format_s"])) {
-         $parsed_record["type"] = $record["dc_format_s"]["attributes"]["value"];
+      if (isset($record["attributes"]["dc_format_s"])) {
+         $parsed_record["type"] = $record["attributes"]["dc_format_s"]["attributes"]["value"];
+      }
+      if (isset($record["attributes"]["dc_description_s"])) {
+        $parsed_record["description"] = $record["attributes"]["dc_description_s"]["attributes"]["value"];
       }
       $parsed_record["url"] = $base_url .  $record["id"];
       array_push($records, $parsed_record);
