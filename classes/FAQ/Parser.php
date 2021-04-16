cd6c464a57f18de9b88b58d1e180f7990015958d
Shaun Ellis <shaun@sdellis.com>
2021-04-16 12:05:12 -0400

    Fixes FAQ 500 error

--- classes/FAQ/Parser.php
+++ classes/FAQ/Parser.php
@@ -9,13 +9,15 @@ class Parser
 {
   public static function convertToFaqRecords($api_response) {
     $records = array();
-    if(count($api_response['search']['results']) > 0) {
-      foreach($api_response['search']['results'] as $result) {
-        array_push($records, new Record($result) );
+    if(isset($api_response['search']['results'])){
+      if(count($api_response['search']['results']) > 0) {
+        foreach($api_response['search']['results'] as $result) {
+          array_push($records, new Record($result) );
+        }
       }
     }
     return $records;
   }
-  
+
 
 }
