2423ac31785b12037c6c82b40348ea65327ec536
Kevin Reiss <kevin.reiss@gmail.com>
2021-12-20 15:18:22 -0600

    First pass to at parsing the basics of hte new DPUL respnose.

--- classes/Blacklight/DpulResponse.php
+++ classes/Blacklight/DpulResponse.php
@@ -13,8 +13,8 @@ class DpulResponse
   public static function getResponse($json_data, $host) {
     $response = array();
     $blacklight_data = json_decode($json_data, true);
-    $response["number"] = $blacklight_data["response"]["pages"]["total_count"];
-    $response["records"] = self::getRecords($blacklight_data["response"]["docs"], $host);
+    $response["number"] = $blacklight_data["meta"]["pages"]["total_count"];
+    $response["records"] = self::getRecords($blacklight_data["data"], $host);
     return $response;
   }
 
@@ -23,23 +23,27 @@ class DpulResponse
     $records = array();
     foreach($record_list as $record) {
       $parsed_record = array();
-      if (isset($record["readonly_title_tesim"])) {
-        $parsed_record["title"] = $record["readonly_title_tesim"];
-      }
-      if (isset($record["readonly_creator_tesim"])) {
-        $parsed_record["contributor"] = $record["readonly_creator_tesim"];
-      }
-      if (isset($record["readonly_publisher_tesim"])) {
-       $parsed_record["origin"] = $record["readonly_publisher_tesim"];
-      }
-      if (isset($record["readonly_collections_tesim"])) {
-       $parsed_record["collection"] = $record["readonly_collections_tesim"];
-      }
-      if (isset($record["readonly_format_tesim"])) {
-        $parsed_record["type"] = $record["readonly_format_tesim"];
-      }
+      // if (isset($record["readonly_title_tesim"])) {
+      //   $parsed_record["title"] = $record["readonly_title_tesim"];
+      // }
+      // if (isset($record["readonly_creator_tesim"])) {
+      //   $parsed_record["contributor"] = $record["readonly_creator_tesim"];
+      // }
+      // if (isset($record["readonly_publisher_tesim"])) {
+      //  $parsed_record["origin"] = $record["readonly_publisher_tesim"];
+      // }
+      // if (isset($record["readonly_collections_tesim"])) {
+      //  $parsed_record["collection"] = $record["readonly_collections_tesim"];
+      // }
+      // if (isset($record["readonly_format_tesim"])) {
+      //   $parsed_record["type"] = $record["readonly_format_tesim"];
+      // }
+      // $parsed_record["id"] = $record["id"];
+      // $parsed_record["url"] = $base_url .  $record["id"];
       $parsed_record["id"] = $record["id"];
+      $parsed_record["type"] = $record["type"];
       $parsed_record["url"] = $base_url .  $record["id"];
+      array_push($records, $parsed_record);
       array_push($records, $parsed_record);
     }
 
